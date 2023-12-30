5f87c75497d29895156370e46baf413ab11905d4
dennismessmer <dennis.messmer@student.reutlingen-university.de>
2023-12-30 14:33:35 +0100

    revert

--- pages/_app.js
+++
@@ -1,5 +0,0 @@
-import '@/styles/global.css' //mit gobals.css aufruf für die post und mit gobal.css aufruf für index (momentan)
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
